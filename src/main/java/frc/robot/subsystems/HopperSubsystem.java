bacc8789881d113c722d5f6f794262105b16a93f
zainmfjavaid <66345508+zainmfjavaid@users.noreply.github.com>
2022-01-11 11:27:40 -0600

    Update HopperSubsystem.java
    Added a shooter motor controller. To have an effective CDC, we have to base our timing on whether the shooter is on and what speed it's rotating at.

--- src/main/java/frc/robot/subsystems/HopperSubsystem.java
+++ src/main/java/frc/robot/subsystems/HopperSubsystem.java
@@ -14,6 +14,7 @@ public class HopperSubsystem extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   private CANSparkMax m_motorController3 = new CANSparkMax(Constants.kHopperMotorThreeID, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private CANSparkMax m_shoooterMotorController = new CANSparkMax(Constants.kShooterMotorID, CANSparkMaxLowLevel.MotorType.kBrushless);
   
   public void HopperSwitch(boolean on) {
     if (on) {
