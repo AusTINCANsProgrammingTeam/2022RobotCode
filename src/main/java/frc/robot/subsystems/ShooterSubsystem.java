b373ad6746fca4144f1020636d7269c8b8e2fa3a
Zane Brotherton <zanebrotherton@gmail.com>
2022-01-11 19:19:53 -0600

    Rough Method Headers, Basic AimMode

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,16 +7,46 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ShooterSubsystem. */
-  public ShooterSubsystem() {}
+  private int AimMode; //0 is LOW, 1 is AUTO, 2 is LAUNCH, 3 is TARMAC
+
+  public ShooterSubsystem() {
+    AimMode = 1;
+  }
+
+  public void adjustHood(double a) {
+    //Adjusts Hood using PID control to passed angle a
+  }
+
+  public void windFlywheel(int rpm) {
+    //Winds Flywheel using PID control to passed rpm
+  }
+
+  public void setAimMode(int m) {
+    AimMode = m;
+  }
+
+  public void cycleAimModeUp() {
+    AimMode++;
+    if(AimMode > 3){AimMode = 0;}
+  }
+
+  public void cycleAimModeDown() {
+    AimMode--;
+    if(AimMode < 0){AimMode = 3;}
+  }
+
+  public double getDistance() {
+    //Uses Limelight to find distance to High Goal
+    return 0.0; //Return distance in feet
+  }
+
+  public void prime() {
+    //Check what aimMode is active, gets distance if AUTO, winds flywheel, adjusts hood correspondingly
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
