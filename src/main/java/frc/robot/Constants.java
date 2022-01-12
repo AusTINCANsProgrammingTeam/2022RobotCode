20e8f06b403b7516d321dafdf10036e889d6e7cb
Zane Brotherton <zanebrotherton@gmail.com>
2022-01-12 17:22:02 -0600

    Added Distance-specific constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,9 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    //Distance Calculation Constants
+    public static final double kLLHeight = 1.5; //Height of the limelight in ft from the carpet
+    public static final double kLLAngle = 30.0; //Angle that the limelight is mounted at
+
+}
