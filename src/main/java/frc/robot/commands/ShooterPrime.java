73d9ff3df9b0062c31af72bc207ebdd739aa45f0
Zane Brotherton <zanebrotherton@gmail.com>
2022-01-22 16:34:31 -0600

    Final debug

--- src/main/java/frc/robot/commands/ShooterPrime.java
+++ src/main/java/frc/robot/commands/ShooterPrime.java
@@ -22,8 +22,7 @@ public class ShooterPrime extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_ShooterSubsystem.windFlywheelTest(3000);
-
+    m_ShooterSubsystem.windFlywheelTest(2000);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
