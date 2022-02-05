d2155223d5d6ed895d25a0244e8d1893b9440f9d
Zane Brotherton <zanebrotherton@gmail.com>
2022-02-05 11:46:02 -0600

    Integrated tx into limelight

--- src/main/java/frc/robot/commands/ShooterPrime.java
+++ src/main/java/frc/robot/commands/ShooterPrime.java
@@ -4,17 +4,21 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class ShooterPrime extends CommandBase {
   private ShooterSubsystem m_ShooterSubsystem;
+  private LimelightSubsystem m_LimelightSubsystem;
   private int i;
 
   /** Creates a new ShooterPrimary. */
-  public ShooterPrime(ShooterSubsystem shooterSubsystem) {
+  public ShooterPrime(ShooterSubsystem shooterSubsystem, LimelightSubsystem limelightSubsystem) {
     addRequirements(shooterSubsystem);
     m_ShooterSubsystem = shooterSubsystem;
+    m_LimelightSubsystem = limelightSubsystem;
+
 
     // Use addRequirements() here to declare subsystem dependencies.
   }
@@ -43,10 +47,12 @@ public class ShooterPrime extends CommandBase {
   @Override
   public boolean isFinished() {
     if(m_ShooterSubsystem.wheelReady()){
-      m_ShooterSubsystem.runCargo(true);
-      i++;
-      if(i==10){ //Expected to add a 200ms delay
-        return true;
+      if(m_LimelightSubsystem.calculatePID() == 0.0){
+        m_ShooterSubsystem.runCargo(true);
+        i++;
+        if(i==10){ //Expected to add a 200ms delay
+          return true;
+        }
       }
     }
     return false;
