4003c7e948a7821c99f28bb58f7025d185d1f66c
Ariya3 <ariya.shah03@gmail.com>
2022-02-26 15:51:03 -0600

    eject command

--- src/main/java/frc/robot/commands/CDSAutoEjectCommand.java
+++ src/main/java/frc/robot/commands/CDSAutoEjectCommand.java
@@ -58,7 +58,9 @@ public class CDSAutoEjectCommand extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intakeReverseCommand.end(true);
+  }
 
   // Returns true when the command should end.
   @Override
