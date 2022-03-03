dc745fee11c5f23705ef296a37783713dc5a8318
Spike <nathanwebb7550@gmail.com>
2022-03-02 19:57:24 -0600

    pid defaults

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -248,7 +248,7 @@ public final class Constants {
   // Climb Constants
   public static final int ClimbMotorOne = 5;
   public static final int ClimbMotorTwo = 12;
-  public static final double[] climbRightPID = {0.25, 0, 0};
-  public static final double[] climbLeftPID = {0.25, 0, 0};
+  public static final double[] climbRightPID = {0.25, 0.005, 1.0};
+  public static final double[] climbLeftPID = {0.25, 0.005, 1.0};
   // public static final int LimitSwitchChannel = 12; // Check what number this needs to be
 }
