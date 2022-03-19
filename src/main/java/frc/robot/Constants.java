e790125e538a18440593f7eb607e2b1960b57fce
zaneb <zanebrotherton@gmail.com>
2022-03-18 22:18:50 -0500

    change belt speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,7 +27,7 @@ public final class Constants {
     },
     EJECT(2500.0, 0.0),
     LAUNCH(0.0, 0.0),
-    TARMAC(2550.0, 0.0),
+    TARMAC(2505.0, 0.0),
     TEST {
       @Override
       public AimModes next() {
@@ -191,7 +191,7 @@ public final class Constants {
   public static final int CDSBeltID = 3;
   public static final int CDSWheelControllerOneID = 2;
   public static final int CDSWheelControllerTwoID = 9;
-  public static final double CDSBeltSpeed = 0.40;
+  public static final double CDSBeltSpeed = 1.0;
   public static final double CDSWheelControllerSpeed = 0.65;
   public static final int frontSensorActivation = 200;
   public static final int middleSensorActivation = 450;
