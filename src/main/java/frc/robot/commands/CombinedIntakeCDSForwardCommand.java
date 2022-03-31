ebfaef53e82b923014c7291d531a3e3d42ae1c79
Adam Vander Pas <adam.vander.pas@ni.com>
2022-03-30 22:33:57 -0500

    spotless

--- src/main/java/frc/robot/commands/CombinedIntakeCDSForwardCommand.java
+++ src/main/java/frc/robot/commands/CombinedIntakeCDSForwardCommand.java
@@ -47,11 +47,11 @@ public class CombinedIntakeCDSForwardCommand extends CommandBase {
   @Override
   public void execute() {
     if (CDSSubsystem.getState() == ManagementState.IDLE) {
-        // If mangement isn't doing anything, run button normally
-        CDSSubsystem.CDSBeltToggle(false);
-        CDSSubsystem.CDSWheelToggle(false);
-        intakeSubsystem.toggleIntake(false);
-        shooterSubsystem.runCargo(Constants.Shooter.cargoReverse);
+      // If mangement isn't doing anything, run button normally
+      CDSSubsystem.CDSBeltToggle(false);
+      CDSSubsystem.CDSWheelToggle(false);
+      intakeSubsystem.toggleIntake(false);
+      shooterSubsystem.runCargo(Constants.Shooter.cargoReverse);
     } else {
       // run ball management if it's in the middle of doing something
       ballManagement.execute();
