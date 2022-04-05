57dd688595ea5a3ce6112741560342c2f0953b5b
loipiop224 <wotiop224@gmail.com>
2022-04-04 19:36:45 -0500

    Fixed climb

--- src/main/java/frc/robot/commands/IntakeDeploy.java
+++ src/main/java/frc/robot/commands/IntakeDeploy.java
@@ -33,6 +33,6 @@ public class IntakeDeploy extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
