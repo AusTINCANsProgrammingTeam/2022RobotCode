4c3292cb9c23e959517b08f5199ab3197fec90c0
loipiop224 <wotiop224@gmail.com>
2022-04-21 19:12:45 -0500

    Adjust Hood Command

---
+++ src/main/java/frc/robot/commands/AdjustHood.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.ShooterSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class AdjustHood extends CommandBase {
+  private ShooterSubsystem m_ShooterSubsystem;
+  /** Creates a new AdjustHood. */
+  public AdjustHood(  ShooterSubsystem shooterSubsystem) {
+    addRequirements(shooterSubsystem);
+    m_ShooterSubsystem = shooterSubsystem;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
+
+    
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    switch(m_ShooterSubsystem.getAimMode()){
+      case TEST: m_ShooterSubsystem.adjustHood(m_ShooterSubsystem.getHoodPositionInput());
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
