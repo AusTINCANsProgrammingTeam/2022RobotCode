a0b266530474a2c5c767352f749e817a49b719b1
zaneb <zanebrotherton@gmail.com>
2022-11-04 18:56:49 -0500

    implement MotorController common class

--- src/main/java/frc/robot/common/hardware/MotorController.java
+++ src/main/java/frc/robot/common/hardware/MotorController.java
@@ -1,115 +1,98 @@
 package frc.robot.common.hardware;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public class MotorController extends CANSparkMax {
+import frc.robot.Constants.MotorDefaults;
 
-  private String mName;
-  private RelativeEncoder mEncoder;
-  private SparkMaxPIDController mPIDController;
+public class MotorController {
+    public static enum MotorConfig {
+        //Drivebase
+        driveLeftFront(13,50),
+        driveLeftRear(14,50),
+        driveRightFront(6,50,true),
+        driveRightRear(7,50,true),
+        //Intake
+        intakeMotor(1,true),
+        intakeDeploy(8),
+        //CDS
+        CDSBelt(3,true),
+        singulatorOne(2,true),
+        singulatorTwo(9),
+        //Shooter
+        shooterOne(10),
+        shooterTwo(11),
+        //Stopper
+        stopperWheel(4),
+        //Climb
+        climbArmOne(5,10),
+        climbArmTwo(12,10,true),
+        climbPoleOne(15,10),
+        climbPoleTwo(16,10,true);
 
-  // PID
-  private double mP;
-  private double mI;
-  private double mD;
+        private int ID;
+        private int currentLimit;
+        private double openLoopRampRate;
+        private boolean reversed;
 
-  // default constructor
-  public MotorController(String name, int deviceID) {
-    super(deviceID, MotorType.kBrushless);
-    mName = name;
-    restoreFactoryDefaults();
+        MotorConfig(int ID, int currentLimit, double openLoopRampRate, boolean reversed){
+            this.ID = ID;
+            this.currentLimit = currentLimit;
+            this.openLoopRampRate = openLoopRampRate;
+            this.reversed = reversed;
+        }
 
-    // Create default values for Spark Max motor controller
-    setSmartCurrentLimit(Constants.defaultCurrentLimit); // default current limit is 40A
-    setIdleMode(CANSparkMax.IdleMode.kCoast); // default mode is Coast
+        MotorConfig(int ID, int currentLimit, double openLoopRampRate){
+            this(ID, currentLimit, openLoopRampRate, false);
+        }
 
-    mPIDController = getPIDController();
-    mEncoder = getEncoder();
-  }
+        MotorConfig(int ID, int currentLimit, boolean reversed){
+            this(ID, currentLimit, MotorDefaults.kOpenLoopRampRate, reversed);
+        }
 
-  // Set PID option for motor controller
-  public MotorController(String name, int deviceID, double[] PID) {
-    this(name, deviceID); // intializes CANSparkMax, Encoder, and PIDController
-    setPID(PID);
-  }
+        MotorConfig(int ID, int currentLimit){
+            this(ID, currentLimit, MotorDefaults.kOpenLoopRampRate, false);
+        }
 
-  public void setPID(double[] PID) {
-    mP = PID[0];
-    mI = PID[1];
-    mD = PID[2];
-    activatePID(PID);
-  }
+        MotorConfig(int ID, boolean reversed){
+            this(ID, MotorDefaults.kCurrentLimit, MotorDefaults.kOpenLoopRampRate, reversed);
+        }
 
-  public SparkMaxPIDController getPIDCtrl() {
-    // Check first that mPIDController has been instantiated
-    if (mPIDController == null) {
-      throw new NullPointerException(
-          "PID Controller for " + this.mName + " has not been instantiated.");
+        MotorConfig(int ID){
+            this(ID, MotorDefaults.kCurrentLimit, MotorDefaults.kOpenLoopRampRate, false);
+        }
+
+        public int getID(){
+            return ID;
+        }
+
+        public int getCurrentLimit(){
+            return currentLimit;
+        }
+
+        public double getOpenLoopRampRate(){
+            return openLoopRampRate;
+        }
+
+        public boolean getReversed(){
+            return reversed;
+        }
     }
-    return mPIDController;
-  }
 
-  public String getName() {
-    return this.mName;
-  }
-
-  // get speeds of wheel side
-  public double getSpeed() {
-    // Check first that mEncoder has been instantiated
-    if (mEncoder == null) {
-      throw new NullPointerException("Encoder for " + this.mName + " has not been instantiated.");
-    }
-    return mEncoder.getVelocity();
-  }
-
-  private void activatePID(double[] PID) {
-    // Check first that mPIDController has been instantiated
-    if (mPIDController == null) {
-      throw new NullPointerException(
-          "PID Controller for " + this.mName + " has not been instantiated.");
+    public static CANSparkMax constructMotor(MotorConfig config){
+        CANSparkMax motor = new CANSparkMax(config.getID(), MotorType.kBrushless);
+        motor.setSmartCurrentLimit(config.getCurrentLimit());
+        motor.setOpenLoopRampRate(motor.getOpenLoopRampRate());
+        motor.setInverted(motor.getInverted());
+        return motor;
     }
 
-    mPIDController.setP(mP);
-    mPIDController.setI(mI);
-    mPIDController.setD(mD);
-
-    // add the values to smart dashboard
-    // SmartDashboard.putNumber(mName + " P value", mP);
-    // SmartDashboard.putNumber(mName + " I value", mI);
-    // SmartDashboard.putNumber(mName + " D value", mD);
-
-    updateSmartDashboard(); // post onto smart dashboard
-  }
-
-  // Updates the Smart Dashboard and checks the PID values to determine if update is needed
-  public void updateSmartDashboard() {
-    if (mPIDController != null) {
-      // TODO: having a sole shuffleboard tab for PID tuning might be beneficical, there's a built
-      // in widget especially for PID
-
-      // check if values were updated in smart dashboard
-      // nothing in it at the start, assign it to value passed in earlier
-      double currentP = SmartDashboard.getNumber(mName + " P value", mP);
-      if (currentP != mP) {
-        mP = currentP;
-        mPIDController.setP(mP);
-      }
-
-      double currentI = SmartDashboard.getNumber(mName + " I value", mI);
-      if (currentI != mI) {
-        mI = currentI;
-        mPIDController.setI(mI);
-      }
-
-      double currentD = SmartDashboard.getNumber(mName + " D value", mD);
-      if (currentD != mD) {
-        mD = currentD;
-        mPIDController.setD(mD);
-      }
+    public static SparkMaxPIDController constructPIDController(CANSparkMax motor, double[] PIDArray){
+      SparkMaxPIDController PIDController = motor.getPIDController();
+      PIDController.setP(PIDArray[0]);
+      PIDController.setI(PIDArray[1]);
+      PIDController.setD(PIDArray[2]);
+      return PIDController;
     }
-  }
 }
